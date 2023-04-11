e050312c7d58793d96209a12cb9feb4819030bb4
Jonathan Carles Irigoitia <jonacarles@gmail.com>
2023-04-11 18:32:34 -0300

    segundo commit

--- proyectojs.js
+++ proyectojs.js
@@ -10,18 +10,20 @@ let camisa = 9500;
 let zapatillas = 24000;
 
 function calcularIVA(precio) {
-    return (precio * 0.21);
+    let precioConIva = precio * 0.21;
+    return (precioConIva);
 }
 
 let elegirProducto;
 do { elegirProducto = prompt ("Elegi tu producto: \n Pantalon 8000$ \n Camisa 9500$ \n Zapatillas 24000$ \n Salir para finalizar")
 if (elegirProducto == "Pantalon") 
-alert("Elegiste Pantalon 8000$ tu total es:" + pantalon + calcularIVA);
+alert("Elegiste Pantalon 8000$ tu total es: " + pantalon + "+" + calcularIVA(pantalon) + " IVA");
 if (elegirProducto == "Camisa") 
-alert("Elegiste Camisa 9500$ tu total es:" + camisa + calcularIVA);
+alert("Elegiste Camisa 9500$ tu total es: " + camisa + "+" + calcularIVA(camisa)+ " IVA");
 if (elegirProducto == "Zapatillas") 
-alert("Elegiste Zapatillas 24000$ tu total es:" + zapatillas + calcularIVA);
+alert("Elegiste Zapatillas 24000$ tu total es: " + zapatillas + "+" + calcularIVA(zapatillas) + " IVA");
 else if (elegirProducto !== "Salir"){
     alert ("Elegi una opcion valida.");
 }
 }while (elegirProducto !== "Salir");
+
