b7311ea28c465f085737d740249dc9e2c65a746d
Jonathan Carles Irigoitia <jonacarles@gmail.com>
2023-05-02 03:09:17 -0300

    segunda preentrega

--- proyectojs.js
+++ proyectojs.js
@@ -1,40 +1,47 @@
+//*PROPAGANDA
+const PROPAGANDA = [
+    { nombre: "Tenemos los mejores productos premium" },
+    { nombre: "Productos premium y clasicos todos los talles" },
+    { nombre: "Seguridad y envios express" }
+]
+
+PROPAGANDA.forEach((textos) => {
+    alert(textos.nombre)
+});
+//* INICIO PROGRAMA USUARIO
 let nombreUsuario = prompt("Bienvenido a tu tienda, ingresa tu nombre de usuario: ");
 if (nombreUsuario == "") {
     alert("Ingresaste un usuario invalido:");
 } else {
     alert("Bienvenido a tu tienda online " + nombreUsuario);
 }
+//*PRODUCTOS
 let productos = [
-    {nombre: "Pantalon", precio: 8000},
-    {nombre: "Camisa", precio: 9500},
-    {nombre: "Zapatillas", precio: 24000}
+    { nombre: "Pantalon", precio: 8000 },
+    { nombre: "Camisa", precio: 9500 },
+    { nombre: "Zapatillas", precio: 24000 },
+    { nombre: "Pantalon premium", precio: 10000 },
+    { nombre: "Camisa premium", precio: 10500 },
+    { nombre: "Zapatillas premium", precio: 26000 }
 ];
 
-
+//*FUNCION IVA
 function sumarIVA(precio) {
     let precioConIva = precio * 1.21;
     return (precioConIva);
 }
 
+//*ELECCION DE PRODUCTOS
 let elegirProducto;
 do {
-    elegirProducto = prompt("Elegi tu producto: \n Pantalon 8000$ \n Camisa 9500$ \n Zapatillas 24000$ \n Salir para finalizar")
-
-    if (elegirProducto == "Pantalon") {
-        alert("Elegiste Pantalon 8000$ tu total es: " + sumarIVA(pantalon) + " con IVA");
+    elegirProducto = prompt("Elegi tu producto: \n 1- Pantalon 8000$ \n 2- Camisa 9500$ \n 3- Zapatillas 24000$ \n 4- Pantalon premium 10000$ \n 5- Camisa premium 10500$ \n 6- Zapatillas premium 26000$  \n 7- Salir para finalizar")
+    let opcionSeleccionada = Number(elegirProducto) - 1;
+    if (opcionSeleccionada >= 0 && opcionSeleccionada < productos.length) {
+        let productoSeleccionado = productos[opcionSeleccionada];
+        alert("Elegiste " + productoSeleccionado.nombre + " " + productoSeleccionado.precio + "$ tu total es: " + sumarIVA(productoSeleccionado.precio) + " con IVA");
     }
-
-    if (elegirProducto == "Camisa") {
-        alert("Elegiste Camisa 9500$ tu total es: " + sumarIVA(camisa) + " con IVA");
-    }
-
-    if (elegirProducto == "Zapatillas") {
-        alert("Elegiste Zapatillas 24000$ tu total es: " + sumarIVA(zapatillas) + " con IVA");
-    }
-    else if (elegirProducto !== "Salir") {
+    else if (elegirProducto !== "7") {
         alert("Elegi una opcion valida.");
     }
-} while (elegirProducto !== "Salir") {
-    alert("Te esperamos pronto!")
-}
-
+} while (elegirProducto !== "7");
+alert("Te esperamos pronto!");
